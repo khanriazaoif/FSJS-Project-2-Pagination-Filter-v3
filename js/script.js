8b27cc282d3a3a9ede6e451dcc3232a79a53f126
Riaz Khan <khanriazaoif@gmail.com>
2018-08-05 12:36:03 -0500

    Added advice from AJ and Lee

--- js/script.js
+++ js/script.js
@@ -23,6 +23,15 @@ FSJS project 2 - List Filter and Pagination
 
 
 
+//AJ advice
+// store the list of students at the top and separate your code into small functions.
+
+// - one the creates the links based on the size of list it gets
+// - one to add them to the DOM
+// - one to filter the list 
+// - one to remove things from the DOM (edited)
 
 
-
+//Lee advice
+// So I think what is giving you trouble is that you aren't using your functions in conjunction with the search feature. Think of it like this: When the page loads your showPage and appendPageLinks functions are called using the initial student list as a parameter. This causes the first 10 students to be displayed and adds the pagination links based on the original student list info. Your search feature doesn't actually change the student list. It merely changes the display style of the students that do not match the search term to none. They are still part of the student list, they just aren't visible. Your pagination links remain unchanged because the pagination function was already called on page load and there is nothing in your search function to cause it to run again.
+// Rather than simply hiding parts of the original student list, I would try to approach it from the perspective of creating a new list of students from those that match the search term. You can then use that list as a parameter and recall the showPage and appendPageLinks functions. (edited)
